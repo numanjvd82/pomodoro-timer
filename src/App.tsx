55ce5323f4a6ab99b6ec697174a5c4482cd68519
Numan <nomanmani62@gmail.com>
2022-02-08 03:06:14 +0500

    25 min timer implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,66 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import Stopwatch from './components/Stopwatch';
 
 function App() {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [reset, setReset] = useState(false);
+  const [start, setStart] = useState(false);
+
+  const handleReset = () => {
+    if (window.confirm('Are you sure you want to reset?')) {
+      setMinutes(25);
+      setSeconds(0);
+      setReset(true);
+    } else {
+      return;
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (start) {
+        if (seconds === 0) {
+          setSeconds(60);
+          setMinutes((minutes) => minutes - 1);
+        }
+        if (minutes === 0 && seconds === 0) {
+          alert('Time is up!');
+          setMinutes(25);
+          setSeconds(60);
+        }
+        setSeconds((seconds) => seconds - 1);
+      }
+      if (!start) {
+        clearInterval(interval);
+      }
+      if (reset) {
+        setMinutes(25);
+        setSeconds(60);
+        setReset(false);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [minutes, seconds, start, reset]);
+
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="heading-1">Pomodoro timer</h1>
+      <div className="container">
+        <Stopwatch minutes={minutes} seconds={seconds} />
+        <section className="btn-container">
+          <button onClick={() => setStart(!start)} className="btn btn__start">
+            {start ? 'Stop' : 'Start'}
+          </button>
+          <button onClick={() => setReset(true)} className="btn btn__reset">
+            Reset
+          </button>
+        </section>
+      </div>
     </>
   );
 }
---
+++ src/components/Stopwatch.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Stopwatch.css';
+
+interface StopwatchProps {
+  minutes: number;
+  seconds: number;
+}
+
+const Stopwatch = ({ minutes, seconds }: StopwatchProps) => {
+  return (
+    <div className="stopwatch">
+      <div className="stopwatch__time stopwatch__minutes">
+        <span className="time stopwatch__minutes-time"> {minutes}</span>
+        <h1 className="label stopwatch__minutes-label">Minutes</h1>
+      </div>
+      <div className="stopwatch__time stopwatch__seconds">
+        <span className="time stopwatch__seconds-time">
+          {seconds === 0 ? `0${seconds}` : `${seconds}`}
+        </span>
+        <h1 className="label stopwatch__seconds-label">Seconds</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
